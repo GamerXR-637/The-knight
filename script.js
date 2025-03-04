f57e74fc70c7186fcdf9f8d234d70e10971fc88c
GamerXR-637 <168220319+GamerXR-637@users.noreply.github.com>
2025-03-03 21:49:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loadingBar = document.getElementById('loading-bar');
+    let width = 0;
+
+    const interval = setInterval(() => {
+        if (width >= 100) {
+            clearInterval(interval);
+            loadingBar.style.display = 'none';
+        } else {
+            width += 10;
+            loadingBar.style.width = width + '%';
+        }
+    }, 400);
+
+    // Add event listener to "Link 1"
+    const link1 = document.getElementById('link1');
+    link1.addEventListener('click', (event) => {
+        event.preventDefault();
+        document.body.style.backgroundColor = 'pink';
+    });
+
+    // Add event listener to "Link 2"
+    const link2 = document.getElementById('link2');
+    link2.addEventListener('click', (event) => {
+        event.preventDefault();
+        document.body.style.backgroundColor = 'purple';
+    });
+
+    // Add event listener to "Link 3"
+    const link3 = document.getElementById('link3');
+    link3.addEventListener('click', (event) => {
+        event.preventDefault();
+        document.body.style.backgroundColor = 'gray';
+    });
+});
